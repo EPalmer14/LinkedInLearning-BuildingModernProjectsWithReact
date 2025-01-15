7baf63f921e378583e6674a198741247ddd39db3
EPalmer14 <zekepalmer14@gmail.com>
2025-01-15 18:02:14 -0500

    another one

--- todo-list-app/src/App.jsx
+++ todo-list-app/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello beautiful people!
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">

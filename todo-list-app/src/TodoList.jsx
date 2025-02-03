63a8dad5b7a14b522f74cfc357f985ce903b396b
EPalmer14 <zekepalmer14@gmail.com>
2025-02-02 23:22:55 -0500

    map todos

--- todo-list-app/src/TodoList.jsx
+++ todo-list-app/src/TodoList.jsx
@@ -4,7 +4,7 @@ import TodoListItem from "./ToDoListItem";
 
 export default function TodoList({ onCompletedClicked, onDeleteClicked, onCreateClicked }) {
   const todos = useSelector(state => state.todos.value);
-  
+
   
   return (
     <div>
@@ -12,11 +12,11 @@ export default function TodoList({ onCompletedClicked, onDeleteClicked, onCreate
       <NewTodoForm onCreateClicked={onCreateClicked}/>
       <p>New Todo form here:</p>
       <h3>Completed:</h3>
-      {completedTodos.map((todo, index) => (
+      {todos.map((todo, index) => (
         <TodoListItem todo={todo} key={index} onDeleteClicked={onDeleteClicked}/>
       ))}
       <h3>Incomplete:</h3>
-      {incompleteTodos.map((todo, index) => (
+      {todos.map((todo, index) => (
         <TodoListItem todo={todo} key={index} onCompletedClicked={onCompletedClicked}/>
       ))}
     </div>

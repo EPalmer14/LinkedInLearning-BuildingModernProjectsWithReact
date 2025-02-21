c760be0d900a0c32ca8e8ba3f3f99d7592ee470d
EPalmer14 <zekepalmer14@gmail.com>
2025-02-20 21:06:40 -0500

    testing styled components

--- todo-list-app/src/TodoListItem.jsx
+++ todo-list-app/src/TodoListItem.jsx
@@ -3,8 +3,16 @@ import { markTodoAsCompleted, deleteTodo } from "./todosSlice";
 import { deleteTodo } from "./thunks";
 import styled from 'styled-components';
 
+const getBackgroundColor = ({ important }) => {
+  if (important) {
+    return 'background-color: yellow;';
+  } else {
+    return 'background-color: white;';
+  }
+}
+
 const CardContainer = styled.div`
-  ${props => props.important && 'background-color: yellow;'}
+  ${getBackgroundColor}
   border-radius: 10px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   padding: 16px;
@@ -13,7 +21,7 @@ const CardContainer = styled.div`
 export default function TodoListItem({ todo }) {
   const dispatch = useDispatch();
   return (
-    <CardContainer important>
+    <CardContainer important={todo.text.endsWith('!')}>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted
